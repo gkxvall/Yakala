f0bddd9cb0ea7e7c499bc61065e49a33cd16dc6c
Vall <gkxvall@users.noreply.github.com>
2026-05-24 02:14:53 +0300

    Initial Commit

---
+++ Yakala/YakalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  YakalaApp.swift
+//  Yakala
+//
+//  Created by gk_vall on 24.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct YakalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YakalaTests/YakalaTests.swift
@@ -0,0 +1,19 @@
+//
+//  YakalaTests.swift
+//  YakalaTests
+//
+//  Created by gk_vall on 24.05.2026.
+//
+
+import Testing
+@testable import Yakala
+
+struct YakalaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
